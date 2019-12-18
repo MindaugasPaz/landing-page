e46b24c9790b42b62b468ff4ce2263f8deedb59d
Mindaugas <m.pazereckas@gmail.com>
2019-12-18 10:22:16 +0100

    Dynamical navBar function

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,13 +18,18 @@
  * 
 */
 
+// const navItem = document.querySelectorAll('section'); //gets all sections
+const myCustomDiv = document.createElement('div');
+const navigationBar = document.querySelector('#navbar__list');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
+// function getSectionName(){
+//     const name = document.getElementsByTagName("section").getAttribute("data-nav");
+// }
 
 
 /**
@@ -33,8 +38,29 @@
  * 
 */
 
-// build the nav
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('the DOM is ready to be interacted with!');
+});
 
+// build the nav
+function buildNavBar(){
+    document.addEventListener('DOMContentLoaded', function () {
+        const sections = document.getElementsByTagName('section');
+        for(let i = 0; i < sections.length; i++){
+            const navItem = document.createElement('li');
+            const name = sections[i].getAttribute('data-nav');
+            const textOfNavigation = document.createTextNode(name);
+            navItem.appendChild(textOfNavigation);
+            navigationBar.appendChild(navItem);
+        }
+        // navigationBar.appendChild(navItem);
+    });
+}
+buildNavBar();
+
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
 
 // Add class 'active' to section when near top of viewport
 
