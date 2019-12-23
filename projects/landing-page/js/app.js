b475d9d7bd0c72c4ab5376b7879936226cb343ae
Mindaugas <m.pazereckas@gmail.com>
2019-12-23 19:47:28 +0200

    make section active and smooth scroll

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -21,63 +21,55 @@
 // const navItem = document.querySelectorAll('section'); //gets all sections
 const myCustomDiv = document.createElement('div');
 const navigationBar = document.querySelector('#navbar__list');
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-// function getSectionName(){
-//     const name = document.getElementsByTagName("section").getAttribute("data-nav");
-// }
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+const sections = document.getElementsByTagName('section');
+const currentSectionArray = Array.from(sections);
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('the DOM is ready to be interacted with!');
+    buildNavBar();
 });
 
 // build the nav
 function buildNavBar(){
-    document.addEventListener('DOMContentLoaded', function () {
-        const sections = document.getElementsByTagName('section');
+       
         for(let i = 0; i < sections.length; i++){
             const navItem = document.createElement('li');
-            const name = sections[i].getAttribute('data-nav');
+            const name = sections[i].dataset.nav;
+
+            let sectionId = sections[i].getAttribute('id');
+            const linkItem = document.createElement('a')
+            linkItem.href = '#' + sectionId; //adds anchor
+            navItem.onclick = function() {
+                event.preventDefault();
+                document.getElementById(sectionId).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            };
+
             const textOfNavigation = document.createTextNode(name);
-            navItem.appendChild(textOfNavigation);
+            navItem.appendChild(linkItem);
+            linkItem.appendChild(textOfNavigation);
             navigationBar.appendChild(navItem);
         }
-        // navigationBar.appendChild(navItem);
-    });
 }
-buildNavBar();
+//check if element is in viewport (https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/)
+function isElementInViewport (currentSection) {
+    return (
+        currentSection.top >= 0 &&
+        currentSection.left >= 0 &&
+        currentSection.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        currentSection.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+};
 
-// $( document ).ready(function() {
-//     console.log( "ready!" );
-// });
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+function handler() {
+    for(let i = 0; i < sections.length; i++) {
+        const currentSection = currentSectionArray[i].getBoundingClientRect();
+        if (isElementInViewport(currentSection)) {
+              sections[i].classList.add('your-active-class');
+         } else {
+               sections[i].classList.remove('your-active-class');
+         }
+     }
+}
+//event to check which section is visible
+window.addEventListener('scroll', handler, false);
